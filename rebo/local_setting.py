90c03e2c4255e557c420ef9718da4c84f6d2348b
saeedfadaoddini <saeedfadaoddini@gmail.com>
2023-09-04 03:30:58 -0700

    num to persian in transaction

--- rebo/local_setting.py
+++ rebo/local_setting.py
@@ -1,14 +1,19 @@
+
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = ['*']
-SECRET_KEY = os.environ.get("SECRET_KEY")
-ZARRINPAL_MERCHANT_ID = os.environ.get("ZARRINPAL_MERCHANT_ID")
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASS = os.environ.get("DB_PASS")
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
-API_MAX_SMS = os.environ.get("API_MAX_SMS")
+
+SECRET_KEY = 'django-insecure-)l7^&$3*+x=-g7b^job8*d(qz98g3b5ahv2!lk*5amuk+7bi#n'
+ZARRINPAL_MERCHANT_ID = 'e701a7c2-73ae-11e8-b07d-005056a205be'
+DB_NAME = 'rebo'
+DB_USER = 'rebo'
+DB_PASS = 'rebo'
+DB_HOST = 'localhost'
+DB_PORT = '5433'
+
+API_MAX_SMS = 'oUSMoSz9YIujHc-XyaanUMimxpSq8rpbx3KHS1nMumA='
+
+
