8ccb319383eaf2352db65903f3e4e1a605468331
saeedfadaoddini <saeedfadaoddini@gmail.com>
2023-12-06 15:25:25 +0330

    update now2

---
+++ rebo/local_setting.py
@@ -0,0 +1,16 @@
+
+# SECURITY WARNING: keep the secret key used in production secret!
+import os
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+API_MAX_SMS = os.environ.get("API_MAX_SMS")
+ZARRINPAL_MERCHANT_ID = os.environ.get("ZARRINPAL_MERCHANT_ID")
+
