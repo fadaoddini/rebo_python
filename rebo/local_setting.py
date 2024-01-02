d9fbab655c94f8714ea480ba1f01aa3e24d776b6
saeedfadaoddini <saeedfadaoddini@gmail.com>
2024-01-02 10:06:49 +0330

    edit profile condition and massege error2

--- rebo/local_setting.py
+++ rebo/local_setting.py
@@ -1,33 +1,16 @@
+
+# SECURITY WARNING: keep the secret key used in production secret!
 import os
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)l7^&$3*+x=-g7b^job8*d(qz98g3b5ahv2!lk*5amuk+7bi#n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
-
-ZARRINPAL_MERCHANT_ID = 'e701a7c2-73ae-11e8-b07d-005056a205be'
-DB_NAME = 'newrebo'
-DB_USER = 'newrebo'
-DB_PASS = 'newrebo'
-DB_HOST = 'localhost'
-DB_PORT = 5432
-
-API_MAX_SMS = 'oUSMoSz9YIujHc-XyaanUMimxpSq8rpbx3KHS1nMumA='
-
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# import os
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-# ALLOWED_HOSTS = ['*']
-# SECRET_KEY = os.environ.get("SECRET_KEY")
-# DB_NAME = os.environ.get("DB_NAME")
-# DB_USER = os.environ.get("DB_USER")
-# DB_PASS = os.environ.get("DB_PASS")
-# DB_HOST = os.environ.get("DB_HOST")
-# DB_PORT = os.environ.get("DB_PORT")
-# API_MAX_SMS = os.environ.get("API_MAX_SMS")
-# ZARRINPAL_MERCHANT_ID = os.environ.get("ZARRINPAL_MERCHANT_ID")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+API_MAX_SMS = os.environ.get("API_MAX_SMS")
+ZARRINPAL_MERCHANT_ID = os.environ.get("ZARRINPAL_MERCHANT_ID")
 
